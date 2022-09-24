8162866378753bb670ab775f4b014cf9c92882b8
Rui Zhou <zhou_ray@outlook.com>
2022-09-23 22:52:51 -0700

    checking in the code for table view

--- MyFavouriteFood/MyFavouriteFood/ViewController.swift
+++ MyFavouriteFood/MyFavouriteFood/ViewController.swift
@@ -7,14 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
 
+    @IBOutlet weak var tableVie: UITableView!
+    
+    let contacts = ["Cooked Fish", "Gongbao JiDing", "Chou Doufu", "Tai Milk", "Coconut Tea", "Boba Tea"];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // test the commit
+        tableVie.delegate = self
+        tableVie.dataSource = self
         
     }
 
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = contacts[indexPath.row]
+        return cell;
+    }
 }
 
