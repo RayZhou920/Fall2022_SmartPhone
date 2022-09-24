bb47d6810b82bd2fa4681a4b8aa5a6fe1a2665f5
Rui Zhou <zhou_ray@outlook.com>
2022-09-23 21:26:24 -0700

    Removing Comments

---
+++ MyFavouriteFood/MyFavouriteFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFavouriteFood
+//
+//  Created by mac on 9/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
